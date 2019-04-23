04ca410d8055a93bc1fd484050d788dad0947c82
jmcasebier <jason.beercase@gmail.com>
2019-04-23 12:11:42 -0500

    set onclick listeners for mainactivity buttons

--- app/src/main/java/com/kyle_jason/imagefinal/MainActivity.java
+++ app/src/main/java/com/kyle_jason/imagefinal/MainActivity.java
@@ -2,12 +2,32 @@ package com.kyle_jason.imagefinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.cameraButton).setOnClickListener(this);
+        findViewById(R.id.drawButton).setOnClickListener(this);
+        findViewById(R.id.openButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+
+        if (viewId == R.id.cameraButton) {
+            //open camera to take picture
+        } else if (viewId == R.id.drawButton) {
+            //open blank canvas for drawing
+        } else if (viewId == R.id.openButton) {
+            //open file system to select image
+        } else {
+            throw new RuntimeException("Not Implemented");
+        }
     }
 }
