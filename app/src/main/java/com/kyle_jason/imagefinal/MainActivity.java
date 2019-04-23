b4ef8c45361d5f1c73ed755cd23e264657e74ee2
jmcasebier <jason.beercase@gmail.com>
2019-04-23 14:37:43 -0500

    setup camera intent - rm guidelines for proper display

--- app/src/main/java/com/kyle_jason/imagefinal/MainActivity.java
+++ app/src/main/java/com/kyle_jason/imagefinal/MainActivity.java
@@ -1,15 +1,28 @@
 package com.kyle_jason.imagefinal;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final int TAKE_PICTURE_CODE = 1;
+    private final int OPEN_IMAGE_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verifyStoragePermissions(this);
 
         findViewById(R.id.cameraButton).setOnClickListener(this);
         findViewById(R.id.drawButton).setOnClickListener(this);
@@ -21,13 +34,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int viewId = view.getId();
 
         if (viewId == R.id.cameraButton) {
-            //open camera to take picture
+            takePicture();
         } else if (viewId == R.id.drawButton) {
             //open blank canvas for drawing
         } else if (viewId == R.id.openButton) {
             //open file system to select image
         } else {
             throw new RuntimeException("Not Implemented");
+        }
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(activity, permissions, 1);
+        }
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, TAKE_PICTURE_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == TAKE_PICTURE_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = findViewById(R.id.imageView);
+            imageView.setImageBitmap(imageBitmap);
         }
     }
 }
