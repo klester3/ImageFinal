4bd771f33264f3f8fa3d874a2df517caef509f9b
jmcasebier <jason.beercase@gmail.com>
2019-04-25 14:47:54 -0500

    correctly displays image in proper orientation

--- app/src/main/java/com/kyle_jason/imagefinal/MainActivity.java
+++ app/src/main/java/com/kyle_jason/imagefinal/MainActivity.java
@@ -2,14 +2,21 @@ package com.kyle_jason.imagefinal;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,7 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView imageView;
     private Bitmap imageBitmap;
     private String photoPath;
-    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,18 +115,101 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivityForResult(Intent.createChooser(intent, "Select Image"), OPEN_IMAGE_CODE);
     }
 
+    private void setImageOrientation(String fileName) {
+        try {
+            ExifInterface ei = new ExifInterface(fileName);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    imageBitmap = rotateImage(imageBitmap, 90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    imageBitmap = rotateImage(imageBitmap, 180);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    imageBitmap = rotateImage(imageBitmap, 270);
+                    break;
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            Log.i("IMG_ERROR", e.getMessage());
+        }
+    }
+
+    private Bitmap rotateImage(Bitmap bitmap, float degrees) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degrees);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(),
+                matrix, true);
+    }
+
+    private String getFilePath(Uri uri) {
+        final boolean needToCheckUri = Build.VERSION.SDK_INT >= 19;
+        String selection = null;
+        String[] selectionArgs = null;
+        if (needToCheckUri && DocumentsContract.isDocumentUri(getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                selection = "_id=?";
+                selectionArgs = new String[]{split[1]};
+            }
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = {MediaStore.Images.Media.DATA};
+            Cursor cursor;
+            try {
+                cursor = getApplicationContext().getContentResolver().query(uri, projection,
+                        selection, selectionArgs, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(column_index);
+                }
+            } catch (Exception e) {
+                Log.i("IMG_ERROR", e.getMessage());
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    private boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    private boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    private boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == TAKE_PICTURE_CODE && resultCode == RESULT_OK) {
-            //need to rotate image as necessary
             imageBitmap = BitmapFactory.decodeFile(photoPath);
+            setImageOrientation(photoPath);
             imageView.setImageBitmap(imageBitmap);
         } else if (requestCode == OPEN_IMAGE_CODE && resultCode == RESULT_OK) {
-            imageUri = data.getData();
+            Uri imageUri = data.getData();
             try {
-                //need to rotate image as necessary
                 InputStream inputStream = getContentResolver().openInputStream(imageUri);
                 imageBitmap = BitmapFactory.decodeStream(inputStream);
+                setImageOrientation(getFilePath(imageUri));
                 imageView.setImageBitmap(imageBitmap);
             } catch (FileNotFoundException e) {
                 Log.i("IMG_ERROR", e.getMessage());
