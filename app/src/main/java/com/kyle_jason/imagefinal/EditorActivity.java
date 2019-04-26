668e83a119cdddd69a943c1220a3c0d791374f13
jmcasebier <jason.beercase@gmail.com>
2019-04-26 14:56:39 -0500

    crop function works...FINALLY!!!

--- app/src/main/java/com/kyle_jason/imagefinal/EditorActivity.java
+++ app/src/main/java/com/kyle_jason/imagefinal/EditorActivity.java
@@ -6,8 +6,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +20,7 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -27,8 +30,10 @@ import java.util.Locale;
 
 public class EditorActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    Bitmap imageBitmap;
+    private ImageView imageView;
+    private Bitmap imageBitmap;
+    private String photoPath;
+    private final int CROP_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,7 +100,7 @@ public class EditorActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case 0:
-                // crop intent
+                cropImage();
                 return true;
             case 1:
                 // show filter dialog
@@ -131,6 +136,61 @@ public class EditorActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "CROP_" + timeStamp;
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/crop/");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File image = new File(storageDir,(imageFileName + ".jpg"));
+        try {
+            image.createNewFile();
+        } catch (IOException e) {
+            Log.i("IMG_ERROR", e.getMessage());
+        }
+
+        photoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void cropImage() {
+        Intent cropIntent = new Intent("com.android.camera.action.CROP");
+        cropIntent.setDataAndType(getImageUri(), "image/*");
+        cropIntent.putExtra("crop", true);
+        cropIntent.putExtra("scaleUpIfNeeded", true);
+        cropIntent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG);
+        if (cropIntent.resolveActivity(getPackageManager()) != null) {
+            File imageFile = null;
+            try {
+                imageFile = createImageFile();
+            } catch (IOException e) {
+                Log.i("IMG_ERROR", e.getMessage());
+            }
+            if (imageFile != null) {
+                Uri imageUri = Uri.fromFile(imageFile);
+                cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(cropIntent, CROP_REQUEST_CODE);
+            }
+        }
+    }
+
+    private Uri getImageUri() {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getApplicationContext()
+                .getContentResolver(), imageBitmap, "CropImage", null);
+        return Uri.parse(path);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CROP_REQUEST_CODE && resultCode == RESULT_OK) {
+            imageBitmap = BitmapFactory.decodeFile(photoPath);
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
