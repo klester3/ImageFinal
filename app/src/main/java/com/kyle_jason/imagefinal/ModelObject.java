7294e4ed2a3832dcbce46d18c874163b1e2f2997
Kyle Lester <klester3@my.apsu.edu>
2019-04-28 16:05:51 -0500

    Working Slideshow . . .

--- app/src/main/java/com/kyle_jason/imagefinal/ModelObject.java
+++ app/src/main/java/com/kyle_jason/imagefinal/ModelObject.java
@@ -2,6 +2,7 @@ package com.kyle_jason.imagefinal;
 
 public enum ModelObject {
 
+    TEST(R.string.test, R.layout.view_image1),
     RED(R.string.red, R.layout.view_red),
     BLUE(R.string.blue, R.layout.view_blue),
     GREEN(R.string.green, R.layout.view_green);
