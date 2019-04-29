e23d1b45a1a7f629b3daf28f8c0f804920373f66
Kyle Lester <klester3@my.apsu.edu>
2019-04-28 19:36:13 -0500

    Working Slideshow . . .

--- app/src/main/java/com/kyle_jason/imagefinal/CustomPagerAdapter.java
+++ app/src/main/java/com/kyle_jason/imagefinal/CustomPagerAdapter.java
@@ -5,22 +5,40 @@ import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class CustomPagerAdapter extends PagerAdapter {
 
     private Context mContext;
+    LayoutInflater mLayoutInflater;
 
     public CustomPagerAdapter(Context context) {
         mContext = context;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public Object instantiateItem(ViewGroup collection, int position) {
-        ModelObject modelObject = ModelObject.values()[position];
+        File dir = new File("/sdcard/SlideFile/");
+        File[] allImages = dir.listFiles();
+
+
+        /*View itemView = mLayoutInflater.inflate(R.layout.pager_item, collection, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(mResources[position]);
+
+        collection.addView(itemView);
+
+        return itemView;
+
+        /*ModelObject modelObject = ModelObject.values()[position];
         LayoutInflater inflater = LayoutInflater.from(mContext);
         ViewGroup layout = (ViewGroup) inflater.inflate(modelObject.getLayoutResId(), collection, false);
         collection.addView(layout);
-        return layout;
+        return layout;*/
     }
 
     @Override
