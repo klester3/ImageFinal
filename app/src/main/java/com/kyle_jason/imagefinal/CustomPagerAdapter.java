09d09cb43c09e0fcda3b29a2e3f2bcc5384819b1
Kyle Lester <klester3@my.apsu.edu>
2019-04-28 20:15:13 -0500

    Still dont work, but it dont crash. . .

--- app/src/main/java/com/kyle_jason/imagefinal/CustomPagerAdapter.java
+++ app/src/main/java/com/kyle_jason/imagefinal/CustomPagerAdapter.java
@@ -10,8 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-
 import java.io.File;
 
 public class CustomPagerAdapter extends PagerAdapter {
